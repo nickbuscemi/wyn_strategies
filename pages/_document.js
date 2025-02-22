fd3f75101f3ad35294d30968b992bea9eaa41161
Nicholas Buscemi <nicholasbuscemi@Nicholass-MBP.fios-router.home>
2025-02-22 10:50:24 -0500

    added video to landing page

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,16 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href='https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Poiret+One&display=swap' rel="stylesheet" />
           <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
+
+          {/* Open Graph Meta Tags */}
+          <meta property="og:title" content="Wyn - Strategies & Marketing Solutions" />
+          <meta property="og:description" content="Expert marketing solutions tailored to your practice." />
+          <meta property="og:image" content="/texticon.png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="741" />
+          <meta property="og:url" content="https://wynstrategies`.com" />
+          <meta property="og:type" content="website" />
+
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
